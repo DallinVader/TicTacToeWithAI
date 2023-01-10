2a063edaf5b289f029fc676c7c518556d13a5a33
DallinVader <dallin@riafox.com>
2023-01-09 22:21:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const squares = document.querySelectorAll('.square');
 let currentPlayer = 'X';
+let gameOver = false;
 
 function checkForWin() {
   // check rows
@@ -35,15 +36,31 @@ function checkForWin() {
   return false;
 }
 
+function checkForDraw() {
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i].textContent === '') {
+      return false;
+    }
+  }
+  return true;
+}
+
 for (let i = 0; i < squares.length; i++) {
   squares[i].addEventListener('click', function (e) {
-    if (e.target.textContent === '') {
+    if (!gameOver && e.target.textContent === '') {
       e.target.textContent = currentPlayer;
       if (checkForWin()) {
         alert(currentPlayer + ' wins!');
+        gameOver = true;
+      } else if (checkForDraw()) {
+        alert('Draw!');
+        gameOver = true;
         window.location.reload();
       }
       currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
     }
   });
 }
+
+// initialize the middle square with a blank space
+squares[4].textContent = '';
